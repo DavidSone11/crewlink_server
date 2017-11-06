eb47d1bc3021974db34710b051641bf0c7a9d84a
santosh <santosh_citech@live.in>
2017-11-06 23:26:32 +0530

    sssssssss

--- mongo_models/users.js
+++ mongo_models/users.js
@@ -2,15 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var userSchema = new mongoose.Schema({
     userName: String,
-    firstName:String,
-    lastName : String,
+    firstName: String,
+    lastName: String,
     password: String,
     email: String,
-    roleCode: {type:String, ref:'role', default:'Planner'},
-    markDelete:{type:Boolean,default:false},
-    createdTime: {type:Date , default:Date.now}
+    roleCode: { type: String, ref: 'role', default: 'Planner' },
+    userProfile: { data: Buffer, contentType: String },
+    markDelete: { type: Boolean, default: false },
+    createdTime: { type: Date, default: Date.now }
 });
 
 
-    
+
 module.exports = mongoose.model('user', userSchema);
